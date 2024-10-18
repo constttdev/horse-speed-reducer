303761cd74ffa844c9f211437b123a99c0e702f7
constttdev <mattheo.hering@gmail.com>
2024-10-18 11:57:49 +0200

    Added lang file and fixed client and file in main folder

--- src/client/java/work/constt/horse_speed_reducer/client/Horse_speed_reducerClient.java
+++ src/client/java/work/constt/horse_speed_reducer/client/Horse_speed_reducerClient.java
@@ -1,10 +1,41 @@
 package work.constt.horse_speed_reducer.client;
 
+import io.netty.buffer.Unpooled;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.entity.attribute.EntityAttributeInstance;
+import net.minecraft.entity.passive.HorseEntity;
+import net.minecraft.text.Text;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.util.Identifier;
+import org.lwjgl.glfw.GLFW;
 
 public class Horse_speed_reducerClient implements ClientModInitializer {
 
+    private static KeyBinding reduceHorseSpeedKey;
+    public static final Identifier SPEED_REDUCE_PACKET_ID = new Identifier("my_mario", "reduce_horse_speed");
+
     @Override
     public void onInitializeClient() {
+        KeyBinding reduceHorseSpeedKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.horse_speed_reducer.reduce_horse_speed",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_G,
+                "category.horse_speed_reducer.keys"
+        ));
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (reduceHorseSpeedKey.wasPressed()) {
+                if (client.player != null && client.player.getVehicle() instanceof net.minecraft.entity.passive.HorseEntity) {
+                    PacketByteBuf buf = new PacketByteBuf(Unpooled.buffer());
+                    ClientPlayNetworking.send(SPEED_REDUCE_PACKET_ID, buf);
+                }
+            }
+        });
     }
 }
--- src/main/java/work/constt/horse_speed_reducer/Horse_speed_reducer.java
+++ src/main/java/work/constt/horse_speed_reducer/Horse_speed_reducer.java
@@ -1,10 +1,46 @@
 package work.constt.horse_speed_reducer;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.minecraft.entity.attribute.EntityAttributeInstance;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.passive.HorseEntity;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 
 public class Horse_speed_reducer implements ModInitializer {
 
+    // Packet identifier for reducing horse speed
+    public static final Identifier SPEED_REDUCE_PACKET_ID = new Identifier("my_mario", "reduce_horse_speed");
+
+    // Minimum speed threshold (set to prevent values like 9.9E-4 or lower)
+    private static final double MINIMUM_SPEED = 0.001;
+
     @Override
     public void onInitialize() {
+        ServerPlayNetworking.registerGlobalReceiver(SPEED_REDUCE_PACKET_ID, (server, player, handler, buf, responseSender) -> {
+            server.execute(() -> {
+                if (player.getVehicle() instanceof HorseEntity) {
+                    HorseEntity horse = (HorseEntity) player.getVehicle();
+                    EntityAttributeInstance speedAttribute = horse.getAttributeInstance(EntityAttributes.GENERIC_MOVEMENT_SPEED);
+                    if (speedAttribute != null) {
+                        double currentSpeed = speedAttribute.getBaseValue();
+                        if (currentSpeed <= MINIMUM_SPEED) {
+                            player.sendMessage(Text.of("§cYou can't decrease the horse's speed any further!"), true);
+                            return;
+                        }
+                        double newSpeed = currentSpeed * 0.98;
+                        if (newSpeed < MINIMUM_SPEED) {
+                            newSpeed = MINIMUM_SPEED;
+                        }
+                        speedAttribute.setBaseValue(newSpeed);
+                        player.sendMessage(Text.of("§aReduced horse speed by 2%! Current speed: " + newSpeed), true);
+                    }
+                } else {
+                    player.sendMessage(Text.of("§cYou are not riding a horse!"), true);
+                }
+            });
+        });
     }
 }
