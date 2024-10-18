50594c92585ce06ac913d11790c7e7b4badfbe9e
constttdev <mattheo.hering@gmail.com>
2024-10-18 11:57:06 +0200

    first commit

---
+++ src/client/java/work/constt/horse_speed_reducer/client/Horse_speed_reducerClient.java
@@ -0,0 +1,10 @@
+package work.constt.horse_speed_reducer.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Horse_speed_reducerClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/work/constt/horse_speed_reducer/Horse_speed_reducer.java
@@ -0,0 +1,10 @@
+package work.constt.horse_speed_reducer;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Horse_speed_reducer implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
